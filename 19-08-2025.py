93a5e1ad81f1c4a214907e13f414614eb4b0ae11
Umakambampati <kambampatiumamaheshwari@gmail.com>
2025-09-20 11:23:35 +0530

    aug

--- 19-08-2025.py
+++ 19-08-2025.py
@@ -1,15 +1,17 @@
-# n=int(input("enter a number"))
-# temp=n
-# l=len(str(n))
-# sum=0
-# while temp>0:
-#     rem=temp%10
-#     sum+=rem**l
-#     temp=temp//10
-# if sum==n:
-#     print("armstrong")
-# else:
-#     print("not armstrong")
+n=int(input("enter a number"))
+temp=n
+l=len(str(n))
+sum=0
+while temp>0:
+    rem=temp%10
+    sum+=rem**l
+    temp=temp//10
+if sum==n:
+    print("armstrong")
+else:
+    print("not armstrong")
+
+    
 def arm_strong(start,end):
     for nums in range(start,end+1):
         temp=nums
